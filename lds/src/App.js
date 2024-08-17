b203ccb6cee97b5b1ec3a8b6e29583e16596f4f9
Barathkumar8610 <barathkumar8610@gmail.com>
2024-08-17 21:46:15 +0530

    Updation6

---
+++ lds/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import EventCarousel from '/Users/mac/Desktop/LDS/lds/src/components/Events.jsx';
+import './header.css';
+
+function App() {
+  return (
+    <div className="App">
+       {/* <div className="header"></div> */}
+       <EventCarousel />
+    </div>
+  );
+}
+
+export default App;
